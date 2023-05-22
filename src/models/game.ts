e967b701c6fb5ed32d928309482990c0ad64f962
Daniela <daniela.scholz93@gmail.com>
2023-05-22 21:07:37 +0200

    cards animation, implement game model, shuffel cards

---
+++ src/models/game.ts
@@ -0,0 +1,34 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+
+    constructor(){
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('clubs_'+ i);
+            this.stack.push('diamonds_'+ i);
+            this.stack.push('hearts_'+ i);
+            this.stack.push('spade_'+ i);
+        }
+        this.shuffleStack();
+    }   
+
+
+    private shuffleStack(): void {
+        function shuffleArray<T>(array: T[]): T[] {
+            const shuffledArray = [...array];
+            for (let i = shuffledArray.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+            }
+            return shuffledArray;
+        }
+
+        this.stack = shuffleArray(this.stack);
+    }
+
+
+}
+
